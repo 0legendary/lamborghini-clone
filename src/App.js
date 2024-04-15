7217f7e0892be7dda93ab0104e3de2883cfaf1fe
Alen.m <alenmullassery123@gmail.com>
2024-04-15 10:07:01 +0530

    Designed the Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Header from "./Components/Header/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
   return (
     <div>
-      
+      <Header/> 
     </div>
   );
 }
