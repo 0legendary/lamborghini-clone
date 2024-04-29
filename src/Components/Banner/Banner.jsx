7334074bfbd88f362941d5ca16adfc26efdbcf60
Alen.m <alenmullassery123@gmail.com>
2024-04-29 23:00:25 +0530

    Edited the Banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import '../Banner/banner.css';
-
+import img from '../../Assets/hero_desk.jpeg'
 function Banner() {
     return (
         <div className='banner'>
-            <div className='banner-heading'>
-                <h2 className='sub-heading'>First Edition</h2>
+            <div className='banner-img'>
+                <img src={img} alt="car" />
             </div>
-            <div className="heading-container">
-                <h1 className='main-heading'>Lamborghini</h1>
-                <h1 className='main-heading'>Arena</h1>
-
-            </div>
-            <div className='icon-container'>
-                <div className='bi bi-hexagon hexagon'></div>
-                <div className='bi bi-hexagon-fill hexagon'></div>
-                <div className='bi bi-chevron-right arrow'></div>
+            <div className='banner-text'>
+                <div className='banner-heading'>
+                    <h2>First Edition</h2>
+                </div>
+                <div className="heading-container">
+                    <h1 className='main-heading'>Lamborghini</h1>
+                    <h1 className='main-heading'>Arena</h1>
+                </div>
+                <div className='icon-container'>
+                    <div className='bi bi-hexagon hexagon'></div>
+                    <div className='bi bi-hexagon-fill hexagon'></div>
+                    <div className='bi bi-chevron-right arrow'></div>
+                </div>
             </div>
         </div>
     );
