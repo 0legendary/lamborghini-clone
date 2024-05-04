c4117057f86980bb2bdc5f596787bcc28a360975
Alen.m <alenmullassery123@gmail.com>
2024-05-04 17:11:45 +0530

    resposived the footer, sticky, and header design

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -35,30 +35,50 @@ function Footer() {
         <div className='footer-container'>
             <div className='footer'>
                 <div className='footer-menu'>
-                    <div className='menu'>
-                        {menus.map((menu) => (
-                            <li>{menu}</li>
+                    <div className='menu menu-lg'>
+                        {menus.map((menu, index) => (
+                            <li key={index}>{menu}</li>
+                        ))}
+                    </div>
+                    <div className='menu menu-sm'>
+                        {menus.slice(0, 6).map((menu, index) => (
+                            <li key={index}>{menu}</li>
+                        ))}
+                    </div>
+                    <div className='menu menu-sm'>
+                        {menus.slice(6).map((menu, index) => (
+                            <li key={index + 6}>{menu}</li>
                         ))}
                     </div>
                     <div className='social-icons'>
-                        {icons.map((icon) => (
-                            <i class={`bi ${icon}`}></i>
+                        {icons.map((icon, index) => (
+                            <i key={index} class={`bi ${icon}`}></i>
                         ))}
                     </div>
                 </div>
-                <div className='des1'>
-                    <p>The consumption and emissions values in the website refer to your geographical IP. This value might be unrealistic if you navigate using VPN or if the position of your Internet provider is imprecise. If you believe you are incorrectly geolocalized, contact your dealer
-                        to get valid consumption and emissions information in your area.
-                    </p>
-                </div>
-                <div className='copyright-details'>
-                    <div className='text'>
-                        <p>Copyright © 2024 Automobili Lamborghini S.p.A. a sole shareholder company part of Audi Group.</p>
-                        <p>All rights reserved. VAT no. IT 00591801204</p>
-                        <p>WARNING ABOUT ILLEGAL OFFERS OF ALLEGED SHARES OF AUTOMOBILI LAMBORGHINI S.P.A.</p>
-                        <p>Automobili Lamborghini S.p.A. got the notice that several third parties across different countries are allegedly offering shares in Automobili Lamborghini S.p.A. These offers are unlawful and originate neither from</p>
-                        <p>Volkswagen Aktiengesellschaft nor from any of its subsidiaries.</p>
+                <div className='footer-base-container'>
+                    <div className='des1'>
+                        <p>The consumption and emissions values in the website refer to your geographical IP. This value might be unrealistic if you navigate using VPN or if the position of your Internet provider is imprecise. If you believe you are incorrectly geolocalized, contact your dealer
+                            to get valid consumption and emissions information in your area.
+                        </p>
                     </div>
+                    <div className='copyright-details'>
+                        <div className='text'>
+                            <p>Copyright © 2024 Automobili Lamborghini S.p.A. a sole shareholder company part of Audi Group.</p>
+                            <p>All rights reserved. VAT no. IT 00591801204</p>
+                            <p>WARNING ABOUT ILLEGAL OFFERS OF ALLEGED SHARES OF AUTOMOBILI LAMBORGHINI S.P.A.</p>
+                            <p>Automobili Lamborghini S.p.A. got the notice that several third parties across different countries are allegedly offering shares in Automobili Lamborghini S.p.A. These offers are unlawful and originate neither from</p>
+                            <p>Volkswagen Aktiengesellschaft nor from any of its subsidiaries.</p>
+                        </div>
+                    </div>
+                    <div className='social-container'>
+                        <div className='social-icons'>
+                            {icons.map((icon, index) => (
+                                <i key={index} class={`bi ${icon}`}></i>
+                            ))}
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
